a8e81aa243f0c6737883944628ea64a37fe6c426
Nikita Minyuk <nikita.minyuk.w@gmail.com>
2021-02-08 23:08:00 +0300

    Imlement GhostsTask.cs file

--- GhostsTask.cs
+++ GhostsTask.cs
@@ -3,29 +3,63 @@ using System.Text;
 
 namespace hashes
 {
-	public class GhostsTask : 
-		IFactory<Document>, IFactory<Vector>, IFactory<Segment>, IFactory<Cat>, IFactory<Robot>, 
+	public class GhostsTask :
+		IFactory<Document>, IFactory<Vector>, IFactory<Segment>, IFactory<Cat>, IFactory<Robot>,
 		IMagic
 	{
+		Cat cat;
+		Document document;
+		byte[] fordocument = Encoding.ASCII.GetBytes("It's a doc");
+		byte[] hacker = Encoding.ASCII.GetBytes("I'm hacker");
+		Vector vector;
+		Segment segment;
+		Robot robot;
+
 		public void DoMagic()
 		{
+			cat?.Rename("Ghostcat");
+			Robot.BatteryCapacity = 100500;
+			vector?.Add(vector);
+			segment?.Start?.Add(segment.Start);
+			if (document != null)
+				for (int i = 0; i < fordocument.Length; i++)
+					fordocument[i] = hacker[i];
 		}
-
-		// Чтобы класс одновременно реализовывал интерфейсы IFactory<A> и IFactory<B> 
-		// придется воспользоваться так называемой явной реализацией интерфейса.
-		// Чтобы отличать методы создания A и B у каждого метода Create нужно явно указать, к какому интерфейсу он относится.
-		// На самом деле такое вы уже видели, когда реализовывали IEnumerable<T>.
 
 		Vector IFactory<Vector>.Create()
 		{
-			throw new NotImplementedException();
+			if (vector == null)
+				vector = new Vector(42, 42);
+			return vector;
 		}
 
 		Segment IFactory<Segment>.Create()
 		{
-			throw new NotImplementedException();
+			if (segment == null)
+				segment = new Segment(new Vector(42, 42), new Vector(42, 42));
+			return segment;
 		}
 
-		// И так даллее по аналогии...
+		Document IFactory<Document>.Create()
+		{
+			if (document == null)
+				document = new Document("CoolDoc", Encoding.UTF8, fordocument); ;
+			return document;
+		}
+
+		Cat IFactory<Cat>.Create()
+		{
+			if (cat == null)
+				cat = new Cat("Nyashka", "cool", DateTime.Now);
+			return cat;
+		}
+
+		Robot IFactory<Robot>.Create()
+		{
+			Robot.BatteryCapacity = 100;
+			if (robot == null)
+				robot = new Robot("42");
+			return robot;
+		}
 	}
 }
